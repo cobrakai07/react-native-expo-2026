56f98732d17397bc3571f0a4387be40d2a8acee7
Kai <dk786161616@gmail.com>
2026-05-21 13:43:32 +0530

    add navigation

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,26 @@
+import "@/global.css"
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+ 
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind...!
+      </Text>
+      <Link href="/onboarding" 
+      className="mt-4 rounded bg-primary text-white p-4">Go to Onboarding Page</Link>
+      <Link href="/(auth)/sign-in" 
+      className="mt-4 rounded bg-primary text-white p-4">Go to Sign in Page</Link>
+      <Link href="/(auth)/sign-up" 
+      className="mt-4 rounded bg-primary text-white p-4">Go to Sign up Page</Link>
+
+      <Link href="/subscriptions/spotify">Spotify Subscription</Link>
+      <Link href={{
+        pathname:"/(tabs)/subscriptions/[id]",
+        params: {id: "claude"}
+      }}>Claude Subscription</Link>
+    </View>
+  );
+}
